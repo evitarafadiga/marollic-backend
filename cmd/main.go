2a344aebc5b3b063e158404fff333cc6c58e57d3
Lázaro Santos <lazvsantos@gmail.com>
2025-01-26 17:35:23 -0300

    jenkins build✅

--- cmd/main.go
+++ cmd/main.go
@@ -7,24 +7,23 @@ import (
 	"go-api/usecase"
 
 	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
 func main() {
 
 	server := gin.Default()
 
+	server.Use(cors.Default())
 	dbConnection, err := db.ConnectDB()
-	if (err != nil) {
+	if err != nil {
 		panic(err)
 	}
 
-	//Camada de repository
 	TaskRepository := repository.NewTaskRepository(dbConnection)
-	//Camada usecase
 	TaskUseCase := usecase.NewTaskUseCase(TaskRepository)
-	//Camada de Controllers
 	TaskController := controller.NewTaskController(TaskUseCase)
-	
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
